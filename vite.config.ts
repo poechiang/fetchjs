1057c8c76662c6b25450d107576b98bc228acd67
云淡风轻 <poechiang@live.cn>
2024-04-21 14:26:51 +0800

    feat: init project template

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import path from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "ReactPkg",
+      formats: ["es", "umd"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "styled-components"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "styled-components": "styled",
+        },
+      },
+    },
+  },
+});
